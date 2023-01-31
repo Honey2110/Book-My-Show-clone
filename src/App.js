377b8fd7552ceef598eba7b446a8ab8c3a598f13
Honey2110 <swapnil21.sk@gmail.com>
2023-02-01 01:30:20 +0530

    Completed Basic Home Page Struture

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+//[React-slick]
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-
+//[pages]
+import Homepage from './pages/Home-page';
+import Moviespage from './pages/Movie-page';
+import Playspage from './pages/Plays-page';
 function App() {
   return (
-    <div >
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <>
+      <Routes>
+        <Route path = "/" element = {<Homepage />}></Route>
+        <Route path = "/movies/:id" element = {<Moviespage/>}></Route>
+        <Route path = "/plays" element = {<Playspage />}></Route>
+     </Routes>
+    </>
   );
 }
 
